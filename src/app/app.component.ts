26be22cc1ccb5fca349de8f26218a5548611c4e6
LeanKhan <eslean20@gmail.com>
2018-07-31 15:45:41 +0100

    Deleted Practice Folder 'Calculator/Keyboard'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Calculator // Sample Project';
+  names: any = {"Name": "Adam",
+"Status": true};
+constructor(){
+
+}
+ngOnInit(){
+document.getElementById("input1").setAttribute("disabled","disabled")
+
+}
+  showArray(){
+    
+  }
+  call(){
+    console.log('Hello world!');
+    console.log(document.getElementById("input1"));
+    this.names.Status = false;
+  }
+
+  
 }
